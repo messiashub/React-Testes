ebafc285ecf4cb8b7183210256be755180e41be3
messiashu <claudineimessias@hotmail.com>
2021-01-27 16:52:42 -0300

    update

--- Testes/my-app/src/App.js
+++ Testes/my-app/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react';
+/* import { render } from 'react-dom'; */
+
+class MeuBotao extends Component {
+    render() {
+        return (
+            <button 
+            onClick={()=>{this.props.click(this.props.label);}}>
+            {this.props.label}
+            </button>
+        )
+    }
 }
+
+class MyLabel extends Component {
+    render() {
+        console.log(this.props);
+        return (
+            <p>{this.props.texto}</p>
+        )
+    }
+}
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            labeltext: '',
+
+        };
+    }
+    setLabelText =(labeltext) =>{
+        this.setState({labeltext});
+    }
+
+
+    render() {
+        console.log(this.state)
+        return (
+            <div>
+                <MyLabel texto={this.state.labeltext} />
+                <p>Clique no: </p>
+
+                <MeuBotao click={this.setLabelText} label="Botao 1" />
+                <MeuBotao click={this.setLabelText} label="Botao 2" />
+                <MeuBotao click={this.setLabelText} label="Botao 3" />
+                <MeuBotao click={this.setLabelText} label="Botao 4" />
+            </div>
+        )
+    }
+
+}
+
+/* Class App extends Component{
+    constructor(props){
+
+    }
+
+    render(){
+        return (
+            <div>
+                <MyLabel texto="Escrevendo pelo App" outro="1" />
+                <p>Olá ,Mundão de Deus</p>
+
+                <MeuBotao label="Botao 1" />
+                <MeuBotao label="Botao 2" />
+                <MeuBotao label="Botao 3" />
+                <MeuBotao label="Botao 4" />
+            </div>
+        );
+    }
+} */
 
 export default App;
