f1f305bb202924a1382d7a6ea58e05130ddef83b
messiashu <claudineimessias@hotmail.com>
2021-01-28 00:15:51 -0300

    update

--- Testes/my-app/src/App.js
+++ Testes/my-app/src/App.js
@@ -3,75 +3,32 @@ import './App.css';
 import React, { Component } from 'react';
 /* import { render } from 'react-dom'; */
 
-class MeuBotao extends Component {
-    render() {
-        return (
-            <button 
-            onClick={()=>{this.props.click(this.props.label);}}>
-            {this.props.label}
-            </button>
-        )
-    }
-}
-
-class MyLabel extends Component {
-    render() {
-        console.log(this.props);
-        return (
-            <p>{this.props.texto}</p>
-        )
-    }
-}
-
-class App extends Component {
-    constructor(props) {
-        super(props);
+class App extends Component{
+    constructor(props){
+        super(props)
 
         this.state = {
-            labeltext: '',
-
-        };
+            marca:'Ford',
+            modelo:'Mustang',
+            color:'vermelho',
+            ano: 1964
+            };
     }
-    setLabelText =(labeltext) =>{
-        this.setState({labeltext});
-    }
-
-
-    render() {
-        console.log(this.state)
-        return (
-            <div>
-                <MyLabel texto={this.state.labeltext} />
-                <p>Clique no: </p>
-
-                <MeuBotao click={this.setLabelText} label="Botao 1" />
-                <MeuBotao click={this.setLabelText} label="Botao 2" />
-                <MeuBotao click={this.setLabelText} label="Botao 3" />
-                <MeuBotao click={this.setLabelText} label="Botao 4" />
-            </div>
-        )
-    }
-
-}
-
-/* Class App extends Component{
-    constructor(props){
-
+    MudaCor =()=>{
+        this.setState({color:"blue"});
     }
 
     render(){
-        return (
+        return(
             <div>
-                <MyLabel texto="Escrevendo pelo App" outro="1" />
-                <p>Olá ,Mundão de Deus</p>
+                <h1>Este é meu {this.state.marca}</h1>
+                <p>Ele possui a cor {this.state.color}</p>
+                <p>O seu modelo é "{this.state.modelo}" {this.state.ano}</p>
 
-                <MeuBotao label="Botao 1" />
-                <MeuBotao label="Botao 2" />
-                <MeuBotao label="Botao 3" />
-                <MeuBotao label="Botao 4" />
+            <button onClick={this.MudaCor}>Mudar cor</button>
+                
             </div>
-        );
+        )
     }
-} */
-
+}
 export default App;
